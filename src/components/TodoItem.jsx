920d6ab6a626e7121bde613df036bfb538024d6c
kaylem williams <kaywil56>
2023-02-24 20:07:09 +1300

    update data

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,20 +1,46 @@
 import DeleteItem from "./DeleteItem";
 import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+import firestore from "../firestore";
 
 const TodoItem = ({ todo }) => {
   const [isEdit, setIsEdit] = useState(false);
-
+  const [editTodo, setEditTodo] = useState({title: "", description: ""});
   // Update todo object on user input
-  const updateInput = (e) => {
-    setTodo({ ...todo, [e.target.name]: e.target.value });
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const todoRef = doc(firestore, "todos", todo.id);
+    await setDoc(
+      todoRef,
+      { description: editTodo.description, title: editTodo.title },
+      { merge: true }
+    );
+    setIsEdit(false)
   };
+
+  const updateInput = (e) => {
+    setEditTodo({ ...editTodo, [e.target.name]: e.target.value });
+  };
+
   return (
     <li>
       {isEdit ? (
-        <>
-          <input onChange={updateInput} placeholder={todo.title} />
-          <textarea onChange={updateInput} placeholder={todo.description} />
-        </>
+        <form onSubmit={handleUpdate}>
+          <input
+            onChange={updateInput}
+            name="title"
+            placeholder={todo.title}
+            value={editTodo.title}
+          />
+          <textarea
+            onChange={updateInput}
+            name="description"
+            placeholder={todo.description}
+            value={editTodo.description}
+          />
+          <button type="submit">Save</button>
+        </form>
       ) : (
         <>
           <h2>{todo.title}</h2>
