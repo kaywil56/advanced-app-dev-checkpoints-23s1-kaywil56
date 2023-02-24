96a16c0dc8eb18734c9df68c893362a31ffedc0b
kaylem williams <kaywil56>
2023-02-24 15:47:52 +1300

    display todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,24 +1,36 @@
+import TodoItem from "./TodoItem";
 import { collection, onSnapshot, query } from "firebase/firestore";
 import { useEffect } from "react";
 import firestore from "../firestore";
+import { useState } from "react";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
   useEffect(() => {
     const fetchTodos = async () => {
       const q = await query(collection(firestore, "todos"));
       onSnapshot(q, (querySnapshot) => {
-        const todos = [];
+        const items = [];
         querySnapshot.forEach((doc) => {
-          todos.push({
+          items.push({
             title: doc.data().title,
             description: doc.data().description,
           });
         });
+        setTodos(items);
       });
     };
     fetchTodos();
   }, []);
-  return <li>hi</li>;
+
+  return (
+    <ul>
+      {todos.map((todo, idx) => {
+        return <TodoItem key={idx} todo={todo} />;
+      })}
+    </ul>
+  );
 };
 
 export default TodoList;
