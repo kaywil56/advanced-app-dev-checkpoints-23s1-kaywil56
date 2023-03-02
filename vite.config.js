cfb627faa88997af0a0243c1034239f142da806c
kaylem williams <kaywil56>
2023-03-03 08:48:49 +1300

    vite pwa service worker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons:[
+          {
+            src: "/icons/icon512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/icons/icon192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
+  ],
+});
